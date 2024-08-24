dce6e69add6b91a015cc274d53660fabc8f6acc6
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-24 09:19:03 -0400

    test findCell method

--- tests/app/labyrinth.test.ts
+++ tests/app/labyrinth.test.ts
@@ -1,4 +1,4 @@
-import { Cells, Labyrinth } from '../../src/app/labyrinth'
+import { CellNotFoundError, Cells, Labyrinth } from '../../src/app/labyrinth'
 
 const cells: Cells = [
   ['S', '0', '1', '0', 'E'],
@@ -17,5 +17,19 @@ describe('Labyrinth Entity', () => {
 
     expect(labyrinth.getCell({ x: 0, y: -1 })).toBe('')
     expect(labyrinth.getCell({ x: 0, y: cells[0].length })).toBe('')
+  })
+
+  test("When I search for 'S', it should return X = 0 and Y = 0", () => {
+    const labyrinth = new Labyrinth(cells)
+
+    expect(labyrinth.findCell('S')).toEqual({ x: 0, y: 0 })
+  })
+
+  test('When a Cell is not found, it should throw an Error', () => {
+    const labyrinth = new Labyrinth(cells)
+
+    const findCell = () => labyrinth.findCell('')
+
+    expect(findCell).toThrow(new CellNotFoundError(''))
   })
 })
