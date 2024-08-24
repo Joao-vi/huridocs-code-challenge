075cce904c6b98558709272e98736f974a504690
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-24 11:17:46 -0400

    add new test cases for Path class

--- tests/app/path-finder-service/path.test.ts
+++ tests/app/path-finder-service/path.test.ts
@@ -10,4 +10,31 @@ describe('Path class', () => {
     expect({ x: path.x, y: path.y }).toEqual({ x: 1, y: 0 })
     expect(path.distance).toBe(1)
   })
+
+  test('When I move UP, it should update the Coordinate and Distance', () => {
+    const path = new Path({ x: 1, y: 0, distance: 0 })
+
+    path.move(DIRECTIONS.UP)
+
+    expect({ x: path.x, y: path.y }).toEqual({ x: 0, y: 0 })
+    expect(path.distance).toBe(1)
+  })
+
+  test('When I move RIGHT, it should update the Coordinate and Distance', () => {
+    const path = new Path({ x: 0, y: 0, distance: 0 })
+
+    path.move(DIRECTIONS.RIGHT)
+
+    expect({ x: path.x, y: path.y }).toEqual({ x: 0, y: 1 })
+    expect(path.distance).toBe(1)
+  })
+
+  test('When I move LEFT, it should update the Coordinate and Distance', () => {
+    const path = new Path({ x: 0, y: 1, distance: 0 })
+
+    path.move(DIRECTIONS.LEFT)
+
+    expect({ x: path.x, y: path.y }).toEqual({ x: 0, y: 0 })
+    expect(path.distance).toBe(1)
+  })
 })
