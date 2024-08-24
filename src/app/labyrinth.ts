3cc34ffa4d4f7fbe27547078e2ef78ae790b09a0
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-24 09:18:49 -0400

    add findCell method to Labyrinth

--- src/app/labyrinth.ts
+++ src/app/labyrinth.ts
@@ -4,6 +4,13 @@ type Cell = string
 
 export type Cells = Cell[][]
 
+export class CellNotFoundError extends Error {
+  constructor(cell: Cell) {
+    super(`The ${cell} has not been found`)
+    this.name = 'CellNotFoundError'
+  }
+}
+
 export class Labyrinth {
   constructor(private readonly cells: Cells) {}
 
@@ -14,5 +21,15 @@ export class Labyrinth {
     if (!isXValid || !isYValid) return ''
 
     return this.cells[coordinate.x][coordinate.y]
+  }
+
+  findCell(cell: Cell): Coordinate {
+    for (let x = 0; x < this.cells.length; x++) {
+      for (let y = 0; y < this.cells[0].length; y++) {
+        if (this.cells[x][y] === cell) return { x, y }
+      }
+    }
+
+    throw new CellNotFoundError(cell)
   }
 }
