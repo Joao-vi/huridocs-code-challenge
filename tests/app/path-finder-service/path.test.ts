5e09d8fabd88e729b0928b353212ca5cac5a1c9e
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-24 11:30:56 -0400

    refactor Path class tests

--- tests/app/path-finder-service/path.test.ts
+++ tests/app/path-finder-service/path.test.ts
@@ -5,36 +5,36 @@ describe('Path class', () => {
   test('When I move DOWN, it should update the Coordinate and Distance', () => {
     const path = new Path({ x: 0, y: 0, distance: 0 })
 
-    path.move(DIRECTIONS.DOWN)
+    const newPath = path.move(DIRECTIONS.DOWN)
 
-    expect({ x: path.x, y: path.y }).toEqual({ x: 1, y: 0 })
-    expect(path.distance).toBe(1)
+    expect({ x: newPath.x, y: newPath.y }).toEqual({ x: 1, y: 0 })
+    expect(newPath.distance).toBe(1)
   })
 
   test('When I move UP, it should update the Coordinate and Distance', () => {
     const path = new Path({ x: 1, y: 0, distance: 0 })
 
-    path.move(DIRECTIONS.UP)
+    const newPath = path.move(DIRECTIONS.UP)
 
-    expect({ x: path.x, y: path.y }).toEqual({ x: 0, y: 0 })
-    expect(path.distance).toBe(1)
+    expect({ x: newPath.x, y: newPath.y }).toEqual({ x: 0, y: 0 })
+    expect(newPath.distance).toBe(1)
   })
 
   test('When I move RIGHT, it should update the Coordinate and Distance', () => {
     const path = new Path({ x: 0, y: 0, distance: 0 })
 
-    path.move(DIRECTIONS.RIGHT)
+    const newPath = path.move(DIRECTIONS.RIGHT)
 
-    expect({ x: path.x, y: path.y }).toEqual({ x: 0, y: 1 })
-    expect(path.distance).toBe(1)
+    expect({ x: newPath.x, y: newPath.y }).toEqual({ x: 0, y: 1 })
+    expect(newPath.distance).toBe(1)
   })
 
   test('When I move LEFT, it should update the Coordinate and Distance', () => {
     const path = new Path({ x: 0, y: 1, distance: 0 })
 
-    path.move(DIRECTIONS.LEFT)
+    const newPath = path.move(DIRECTIONS.LEFT)
 
-    expect({ x: path.x, y: path.y }).toEqual({ x: 0, y: 0 })
-    expect(path.distance).toBe(1)
+    expect({ x: newPath.x, y: newPath.y }).toEqual({ x: 0, y: 0 })
+    expect(newPath.distance).toBe(1)
   })
 })
