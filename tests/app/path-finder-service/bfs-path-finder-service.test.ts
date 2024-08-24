b5b54ddbde567252eb47b06e416e6c73f93d2614
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-24 11:38:26 -0400

    add BFSPathFinderService tests

---
+++ tests/app/path-finder-service/bfs-path-finder-service.test.ts
@@ -0,0 +1,76 @@
+import { CellNotFoundError, Cells, Labyrinth } from '../../../src/app/labyrinth'
+import { BFSPathFinderService } from '../../../src/app/path-finder-service/bfs-path-finder-service'
+
+const THEE_STEPS_LABYRINTH: Cells = [
+  ['1', 'S', '0', '0', 'E'],
+  ['1', '0', '1', '0', '1'],
+  ['1', '0', '0', '0', '0'],
+  ['0', '0', '1', '1', '1'],
+  ['0', '0', '0', '0', '0'],
+]
+
+const EIGHT_STEPS_LABYRINTH: Cells = [
+  ['S', '0', '1', '0', 'E'],
+  ['1', '0', '1', '0', '1'],
+  ['1', '0', '0', '0', '0'],
+  ['0', '0', '1', '1', '1'],
+  ['0', '0', '0', '0', '0'],
+]
+
+const UNSOLVABLE_LABYRINTH: Cells = [
+  ['S', '0', '1', '0', '0'],
+  ['1', '0', '1', '0', '1'],
+  ['1', '0', '0', '0', '0'],
+  ['0', '0', '1', '1', '1'],
+  ['0', '0', '0', '0', '0'],
+]
+
+const INVALID_LABYRINTH: Cells = [
+  ['0', '0', '1', '0', '0'],
+  ['1', '0', '1', '0', '1'],
+  ['1', '0', '0', '0', '0'],
+  ['0', '0', '1', '1', '1'],
+  ['0', '0', '0', '0', '0'],
+]
+
+describe('BFSPathFinderService', () => {
+  test('When given a valid Labyrinth, it should return the correct steps to solve it', () => {
+    const labyrinth = new Labyrinth(THEE_STEPS_LABYRINTH)
+
+    const bfsPathFinderService = new BFSPathFinderService()
+
+    const result = bfsPathFinderService.execute(labyrinth)
+
+    expect(result).toBe(3)
+  })
+
+  test('When given a valid eight steps Labyrinth, it should return the correct steps to solve it', () => {
+    const labyrinth = new Labyrinth(EIGHT_STEPS_LABYRINTH)
+
+    const bfsPathFinderService = new BFSPathFinderService()
+
+    const result = bfsPathFinderService.execute(labyrinth)
+
+    expect(result).toBe(8)
+  })
+
+  test('When given a unsolvable Labyrinth, it should return -1', () => {
+    const labyrinth = new Labyrinth(UNSOLVABLE_LABYRINTH)
+
+    const bfsPathFinderService = new BFSPathFinderService()
+
+    const result = bfsPathFinderService.execute(labyrinth)
+
+    expect(result).toBe(-1)
+  })
+
+  test('When given a invalid Labyrinth, it should throw an Error', () => {
+    const labyrinth = new Labyrinth(INVALID_LABYRINTH)
+
+    const bfsPathFinderService = new BFSPathFinderService()
+
+    const result = () => bfsPathFinderService.execute(labyrinth)
+
+    expect(result).toThrow(new CellNotFoundError('S'))
+  })
+})
