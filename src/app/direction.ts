f365289220082c9845b0b85f088d032139a93101
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-24 09:27:08 -0400

    add directions

---
+++ src/app/direction.ts
@@ -0,0 +1,15 @@
+import { Coordinate } from './coordinate'
+
+type Direction = Coordinate
+
+const UP: Direction = { x: -1, y: 0 }
+const DOWN: Direction = { x: 1, y: 0 }
+const LEFT: Direction = { x: 0, y: -1 }
+const RIGHT: Direction = { x: 0, y: 1 }
+
+export const DIRECTIONS = {
+  UP,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
