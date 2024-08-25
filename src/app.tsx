16f2d2dea211edab3f5449efd871f703e678fd29
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-25 09:22:07 -0400

    install and setup react on vite

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div>
+      {counter}
+      <button onClick={() => setCounter(counter + 1)}>+</button>
+    </div>
+  )
+}
+
+export const App = () => (
+  <div>
+    <Counter />
+    App
+  </div>
+)
