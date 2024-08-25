6208d84fd74e0924bf8659b289c5b651286e3eee
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-25 16:19:31 -0400

    add cleanup function for setup test

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom'
+import { cleanup } from '@testing-library/react-hooks'
+import { afterEach } from 'vitest'
+
+// runs a clean after each test case (e.g. clearing jsdom)
+afterEach(() => {
+  cleanup()
+})
