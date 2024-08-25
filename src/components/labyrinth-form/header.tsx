ae46448bf083ecc6b661a90207b226e6cf6a39af
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-25 10:34:47 -0400

    add labyrinth's Header component

---
+++ src/components/labyrinth-form/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => (
+  <header className="bg-green-700 p-4 rounded-xl shadow-md">
+    <h1 className="text-4xl text-white font-semibold">Labyrinth Escape</h1>
+    <span className="">
+      Got tired of solving maze by yourself? Fill out the fields bellow and get the solution on one
+      click of distance.
+    </span>
+  </header>
+)
