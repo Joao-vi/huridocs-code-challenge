e9166e23fcac011951f9aaba5ddcb8e7a70522e2
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-25 11:34:07 -0400

    add react testing library

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true, // Enables Jest-like global test functions (e.g., `describe`, `it`)
-    environment: 'node', // Simulates a DOM environment (useful for React components)
+    environment: 'jsdom', // Simulates a DOM environment (useful for React components)
     include: ['tests/**/*.{test,spec}.{js,ts,jsx,tsx}'], // Match test files
     coverage: {
       reporter: ['text', 'json', 'html'], // Coverage reports
     },
+    setupFiles: './tests/setup.ts',
   },
 })
