f90049a55cad278a0043b1bc10b93d7bbee41953
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-25 15:02:08 -0400

    add changeCell to Toolbar controls

--- src/components/labyrinth-form/toolbar.tsx
+++ src/components/labyrinth-form/toolbar.tsx
@@ -1,16 +1,41 @@
 import { MagicWand } from '@phosphor-icons/react'
 import { Button } from '../ui/button'
+import { useLabyrinthContext } from './core/context'
 
-export const Toolbar = () => (
-  <div className="flex items-center gap-3 p-3 border border-zinc-200 rounded-xl">
-    <Button variant="secondary">Start</Button>
-    <Button variant="secondary">Path</Button>
-    <Button variant="secondary">Wall</Button>
-    <Button variant="secondary">Exit</Button>
+export const Toolbar = () => {
+  const { changeCell } = useLabyrinthContext()
 
-    <Button>
-      Solve it
-      <MagicWand weight="bold" />
-    </Button>
-  </div>
-)
+  return (
+    <div className="flex items-center gap-3 p-3 border border-zinc-200 rounded-xl">
+      <Button
+        variant="secondary"
+        onClick={() => changeCell('S')}
+      >
+        Start
+      </Button>
+      <Button
+        variant="secondary"
+        onClick={() => changeCell('0')}
+      >
+        Path
+      </Button>
+      <Button
+        variant="secondary"
+        onClick={() => changeCell('1')}
+      >
+        Wall
+      </Button>
+      <Button
+        variant="secondary"
+        onClick={() => changeCell('E')}
+      >
+        Exit
+      </Button>
+
+      <Button>
+        Solve it
+        <MagicWand weight="bold" />
+      </Button>
+    </div>
+  )
+}
