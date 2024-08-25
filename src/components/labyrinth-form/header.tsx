fe021b4b7f51be7d5ed55632101f98d16170264a
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-25 15:37:17 -0400

    change header's subtitle

--- src/components/labyrinth-form/header.tsx
+++ src/components/labyrinth-form/header.tsx
@@ -2,8 +2,8 @@ export const Header = () => (
   <header className="bg-green-700 p-4 rounded-xl shadow-md">
     <h1 className="text-4xl text-white font-semibold">Labyrinth Escape</h1>
     <span className="">
-      Got tired of solving maze by yourself? Fill out the fields bellow and get the solution on one
-      click of distance.
+      Got tired of solving mazes by yourself? Fill out the fields below and get the solution with
+      just one click.
     </span>
   </header>
 )
