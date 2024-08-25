14cc64354336badd75dcefd52aed9dbcec26dfd2
joao-vi <joaovictor.go.ro@gmail.com>
2024-08-25 15:36:37 -0400

    add ErrorMessage component

---
+++ src/components/labyrinth-form/form/error-message.tsx
@@ -0,0 +1,12 @@
+import { Confetti, SealWarning } from '@phosphor-icons/react'
+import { ComponentProps } from 'react'
+
+type ErrorMessageProps = ComponentProps<'span'>
+
+export const ErrorMessage = (props: ErrorMessageProps) => (
+  <div className="flex items-center gap-2 p-3 rounded-lg bg-red-800 text-white">
+    <SealWarning weight="bold" />
+
+    <span {...props} />
+  </div>
+)
